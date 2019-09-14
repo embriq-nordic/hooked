ad195d2f7aebfd0cab20b82fe1e1f591cd8de590
Lars Wefald <lars.wefald@embriq.no>
2019-09-14 20:50:53 +0200

    Generate sample data.

--- cmd/sample-data/main.go
+++ cmd/sample-data/main.go
@@ -9,6 +9,8 @@ import (
 	"github.com/rejlersembriq/hooked/pkg/dynamo"
 	"github.com/rejlersembriq/hooked/pkg/participant"
 	"log"
+	"math/rand"
+	"strconv"
 )
 
 const (
@@ -17,7 +19,79 @@ const (
 	tableName = "hooked-participants"
 )
 
+const (
+	noToGenerate = 25
+	maxScore     = 100
+)
+
+const (
+	compA comapany = iota
+	compB
+	compC
+	compD
+	compE
+
+	noOfCompanies = 5
+)
+
+const (
+	commentA comment = iota
+	commentB
+	commentC
+	commentD
+
+	noOfComments = 4
+)
+
+type comapany uint8
+
+func (c comapany) String() string {
+	switch c {
+	case compA:
+		return "CompanyA"
+	case compB:
+		return "CompanyB"
+	case compC:
+		return "CompanyC"
+	case compD:
+		return "CompanyD"
+	case compE:
+		return "CompanyE"
+	default:
+		panic(fmt.Sprintf("Invalid value for company: %d", int8(c)))
+	}
+}
+
+type comment uint8
+
+func (c comment) String() string {
+	switch c {
+	case commentA:
+		return "Some comment."
+	case commentB:
+		return "Some other comment."
+	case commentC:
+		return ""
+	case commentD:
+		return "This is a slightly longer comment than the others."
+	default:
+		panic(fmt.Sprintf("Invalid value for comment: %d", int8(c)))
+	}
+}
+
+func getPhoneNo(someInt int, len int) string {
+	var phoneNo string
+
+	for i := 0; i < len; i++ {
+		phoneNo += strconv.Itoa(someInt % 10)
+		someInt = someInt / 10
+	}
+
+	return phoneNo
+}
+
 func main() {
+	rand.Seed(42) // "To get the same randomness each time"
 	conf, err := external.LoadDefaultAWSConfig(external.WithSharedConfigProfile(profile))
 	if err != nil {
 		log.Fatalf("Message getting AWS config: %v", err)
@@ -28,50 +102,25 @@ func main() {
 
 	repo := dynamo.New(ddb, tableName)
 
-	p := &participant.Participant{
-		//Id:      "5991dd0d-da1e-4088-9889-72067ab9d467",
-		Email:   "larwef@gmail.com",
-		Name:    "Lars Wefald",
-		Phone:   "12345678",
-		Org:     "Rejlers Embriq",
-		Comment: "test",
-		Score:   1,
-	}
+	for i := 0; i < 25; i++ {
+		r := rand.Int()
 
-	res, err := repo.Save(p)
-	if err != nil {
-		log.Fatalf("Error saving participant: %v", err)
-	}
-
-	bytes, err := json.MarshalIndent(res, "", "    ")
-	if err != nil {
-		log.Fatalf("Error marshalling result: %v", err)
-	}
-
-	fmt.Printf("Save:\n%s\n", string(bytes))
-
-	get, err := repo.Get(res.Id)
-	if err != nil {
-		log.Fatalf("Error getting participant: %v", err)
-	}
-
-	bytes, err = json.MarshalIndent(get, "", "    ")
-	if err != nil {
-		log.Fatalf("Error marshalling result: %v", err)
-	}
-
-	fmt.Printf("Get:\n%s\n", string(bytes))
-
-	participants, err := repo.GetAll()
-	if err != nil {
-		log.Fatalf("Error getting participans: %v", err)
-	}
-	for i, p := range participants {
-		bytes, err = json.MarshalIndent(p, "", "    ")
-		if err != nil {
-			log.Fatalf("Error marshalling result: %v", err)
+		p := &participant.Participant{
+			Email:   fmt.Sprintf("Participant%d@%s.com", i, comapany(r%noOfCompanies).String()),
+			Name:    fmt.Sprintf("Participant%d", i),
+			Phone:   getPhoneNo(r, 8),
+			Org:     comapany(r % noOfCompanies).String(),
+			Comment: comment(r % noOfComments).String(),
+			Score:   r % maxScore,
 		}
 
-		fmt.Printf("Get all %d:\n%s\n", i, string(bytes))
+		saved, err := repo.Save(p)
+		if err != nil {
+			log.Fatalf("Error saving participant: %v", err)
+		}
+
+		bytes, _ := json.MarshalIndent(saved, "", "    ")
+		fmt.Printf("Saved:\n%s\n", string(bytes))
+
 	}
 }
